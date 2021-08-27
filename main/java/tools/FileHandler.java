73ee7962e221a5cec06138d4d776983ad654d495
Livio <193267+javalc6@users.noreply.github.com>
2021-08-27 21:17:20 +0200

    Delete main directory

--- main/java/tools/FileHandler.java
+++
@@ -1,9 +0,0 @@
-package tools;
-
-import org.json.JSONException;
-
-public interface FileHandler {
-    String getMimeType();
-    String encodeFile() throws JSONException;
-    boolean decodeFile(String content) throws JSONException;
-}
